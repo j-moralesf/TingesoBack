fbcfb961236fd5f1f8eb01e148033016545b7bc2
Joaquin Morales <joaquin.morales.f@usach.cl>
2022-01-16 01:37:48 -0300

    test

--- TingesoEntrega2/src/test/java/com/example/demo/TingesoEntrega2ApplicationTests.java
+++ TingesoEntrega2/src/test/java/com/example/demo/TingesoEntrega2ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+import java.util.List;
+
+import com.example.demo.Repositorios.Producto_Repositorio;
+import com.example.demo.Servicios.Producto_servicio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@DisplayName("PruebasUnitarias")
+@Tag("unit")
 class TingesoEntrega2ApplicationTests {
+	@InjectMocks
+	Producto_servicio servicio;
+	@Mock
+	Producto_Repositorio repositorio;
 
 	@Test
+	@DisplayName("Prueba 1")
 	void contextLoads() {
+		int prueba = 2;
+		assertEquals(prueba,2);
 	}
 
 }
